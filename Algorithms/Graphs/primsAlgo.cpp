f61dcec7d7953187fcd37ee6f9e4519dc2bb231e
sg28march <52504382+sg28march@users.noreply.github.com>
2019-12-29 14:56:25 +0530

    Prims algorithm using c++ stl

---
+++ Algorithms/Graphs/primsAlgo.cpp
@@ -0,0 +1,119 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+# define INF 0x3f3f3f3f
+ 
+// iPair ==>  Integer Pair
+typedef pair<int, int> iPair;
+ 
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
+{
+    int V;    // No. of vertices
+ 
+    
+    list< pair<int, int> > *adj;
+ 
+public:
+    Graph(int V);  // Constructor
+ 
+   
+    void addEdge(int u, int v, int w);
+ 
+  
+    void primMST();
+};
+ 
+// Allocates memory for adjacency list
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<iPair> [V];
+}
+ 
+void Graph::addEdge(int u, int v, int w)
+{
+    adj[u].push_back(make_pair(v, w));
+    adj[v].push_back(make_pair(u, w));
+}
+ 
+// Prints shortest paths from src to all other vertices
+void Graph::primMST()
+{
+    
+    priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
+ 
+    int src = 0; // Taking vertex 0 as source
+ 
+    vector<int> key(V, INF);
+ 
+    
+    vector<int> parent(V, -1);
+ 
+    
+    vector<bool> inMST(V, false);
+ 
+   
+   
+    pq.push(make_pair(0, src));
+    key[src] = 0;
+ 
+    /* Looping till priority queue becomes empty */
+    while (!pq.empty())
+    {
+        
+        int u = pq.top().second;
+        pq.pop();
+ 
+        inMST[u] = true;  // Include vertex in MST
+ 
+        // 'i' is used to get all adjacent vertices of a vertex
+        list< pair<int, int> >::iterator i;
+        for (i = adj[u].begin(); i != adj[u].end(); ++i)
+        {
+            
+            int v = (*i).first;
+            int weight = (*i).second;
+ 
+           
+            if (inMST[v] == false && key[v] > weight)
+            {
+                
+                key[v] = weight;
+                pq.push(make_pair(key[v], v));
+                parent[v] = u;
+            }
+        }
+    }
+ 
+    // Print edges of MST using parent array
+    for (int i = 1; i < V; ++i)
+        printf("%d - %d\n", parent[i], i);
+}
+ 
+// Driver program to test methods of graph class
+int main()
+{
+    // create the graph given in above fugure
+    int V = 9;
+    Graph g(V);
+ 
+    //  making above shown graph
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(5, 6, 2);
+    g.addEdge(6, 7, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+ 
+    g.primMST();
+ 
+    return 0;
+}
